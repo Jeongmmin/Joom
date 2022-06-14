95ab068b63929d2acf7928633b175ab6468e7a94
Jeongmmin <seo88kr@hanmail.net>
2022-06-14 23:49:40 +0900

    make basic chat

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,48 @@
-alert('hi✋🏻');
+const messageList = document.querySelector('ul');
+const nickFrom = document.querySelector('#nickname');
+const messageForm = document.querySelector('#message');
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = {type, payload};
+  return JSON.stringify(msg);
+}
+
+// 메세지 받기
+socket.addEventListener('open', () => {
+  console.log('connented to Server ✅');
+});
+
+socket.addEventListener('message', (message) => {
+  const li = document.createElement('li');
+  li.innerHTML = message.data;
+  messageList.append(li);
+  // console.log('New message: ', message.data);
+});
+
+socket.addEventListener('close', () => {
+  console.log('Disconnented from Server ❌');
+});
+
+// setTimeout(() => {
+//   socket.send('hello from the browser!');
+// }, 10000);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector('input');
+  socket.send(makeMessage('new_message', input.value));
+  input.value = '';
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickFrom.querySelector('input');
+  socket.send(makeMessage('nickname', input.value));
+  alert('success!✅')
+  input.value = '';
+}
+
+messageForm.addEventListener('submit', handleSubmit);
+nickFrom.addEventListener('submit', handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -15,6 +15,32 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
 
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({
+  server
+});
+
+// function handleConnection(socket) {
+//   console.log(socket);
+// }
+
+const sockets = [];
+
+wss.on('connection', (socket) => {
+  sockets.push(socket);
+  socket['nickname'] = '👻';
+  console.log('connented to Browser ✅');
+  socket.on('close', () => console.log('Disconnented from Browser ❌'));
+  socket.on('message', (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case 'new_message':
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname} : ${message.payload}`)
+        );
+      case 'nickname':
+        socket['nickname'] = message.payload;
+    }
+  });
+});
 
 server.listen(3000, handleListen);
