cf119cd9f702eeaf72fd6d763e23d37068fefca5
Jeongmmin <seo88kr@hanmail.net>
2022-06-30 23:37:44 +0900

    Recap & Study socketIo document

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,15 +1,17 @@
 const socket = io();
 
-const welcon = document.querySelector('welcome');
-const form = welcome.querySelector('form');
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+function backendDone(msg) {
+  console.log(`The backend says: `, msg);
+}
 
 function handleRoomSubmit(event) {
   event.preventDefault();
-  const input = form.querySelector('input');
-  socket.emit('enter_room', { payload: input.value }, () => {
-    console.log('server is done!');
-  });
-  input.value = '';
+  const input = form.querySelector("input");
+  socket.emit("enter_room", input.value, backendDone);
+  input.value = "";
 }
 
 form.addEventListener('submit', handleRoomSubmit);
